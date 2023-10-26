bd56d7fb8a2b9a85cd1e6bae722c39d91ad0f5cd
dberrios <d.berrios6@alumnos.santotomas.cl>
2023-10-26 18:31:26 -0300

    Taller10%

--- app/src/main/java/com/example/gpsmapas/Video.java
+++ app/src/main/java/com/example/gpsmapas/Video.java
@@ -2,14 +2,42 @@ package com.example.gpsmapas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class Video extends AppCompatActivity {
+
+    private VideoView videito;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
-        VideoView MiVideito = findViewById(R.id.videoView);
+
+        Button btn_main = findViewById(R.id.btn_main);
+
+        videito = findViewById(R.id.videoView);
+
+        String videoUrl ="https://drive.google.com/file/d/1HRwfTfVRiR1r4OjzhVQoUjYFMy-V33T8/view?usp=share_link";
+
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videito);
+        videito.setMediaController(mediaController);
+
+        videito.setVideoURI(Uri.parse(videoUrl));
+
+        videito.start();
+
+        btn_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
     }
 }
